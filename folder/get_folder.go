2fa8bbab0db6a0e1b6223575d2803253cde6716d
dillondesilva <dillon.desilva@geeksdiary.com>
2024-10-02 15:29:44 +1000

    Began implementation of GetAllChildFolders by configuring necessary parameters for traversal. This will be done inline to start opposed to creating a series of FolderTreeNodes. Wrote a method to get the subfolders directly underneath/adjacent to a given folder

--- folder/get_folder.go
+++ folder/get_folder.go
@@ -1,6 +1,11 @@
 package folder
 
-import "github.com/gofrs/uuid"
+import (
+	"github.com/gofrs/uuid"
+	"fmt"
+	"slices"
+	"strings"
+)
 
 func GetAllFolders() []Folder {
 	return GetSampleData()
@@ -17,11 +22,69 @@ func (f *driver) GetFoldersByOrgID(orgID uuid.UUID) []Folder {
 	}
 
 	return res
+}
 
+func GetChildFoldersOneLevelDown(givenFolders []Folder, rootFolder Folder) []Folder {
+	// Get subfolders
+	rootPath := rootFolder.Paths
+	rootFolderDepth := len(strings.Split(rootPath, "."))
+	childFoldersOneLevelDown := []Folder{}
+
+	for _, folder := range givenFolders {
+		if strings.HasPrefix(folder.Paths, rootPath) {
+			folderDepth := len(strings.Split(folder.Paths, "."))
+			if rootFolderDepth + 1 == folderDepth {
+				childFoldersOneLevelDown = append(childFoldersOneLevelDown, folder)
+			}
+		}
+	}
+
+	return childFoldersOneLevelDown
 }
 
 func (f *driver) GetAllChildFolders(orgID uuid.UUID, name string) []Folder {
-	// Your code here...
+	/*
+	Approach: First get all the folders for the given OrgID by calling
+	GetFoldersByOrgID. Then create an empty array for unseen folders,
+	seen paths and the child folders. Also create a variable to store
+	the traversal's current parent folder which is initially
+	set to whatever the given name of a folder's Paths value is.
+	Perform a DFS, adding to the child folders array and return
+	when there are no more unseen folders.
+	*/
 
+	// TODO: Validation of function parameters
+
+	foldersInOrgID := f.GetFoldersByOrgID(orgID)
+	idxOfFolderDataFromParams := slices.IndexFunc(foldersInOrgID, func(folderData Folder) bool {
+		if folderData.Name == name {
+			return true
+		}
+
+		return false
+	})
+	folderDataFromParams := foldersInOrgID[idxOfFolderDataFromParams]
+	fmt.Println(folderDataFromParams)
+
+	fmt.Println("Getting child folders one level down from the root")
+	childFoldersOneLevelDown := GetChildFoldersOneLevelDown(foldersInOrgID, folderDataFromParams)
+	fmt.Printf("%v\n", childFoldersOneLevelDown)
+
+	// seenPathsDuringTraversal := []Folder{}
+	// unseenPathsDuringTraversal := []Folder{ folderDataFromParams }
+	// allChildFolders := []Folder{}
+	// currentParentPathDuringTraversal := folderDataFromParams.Paths
+
+	// for len(unseenFoldersDuringTraversal) > 0 {
+	// 	// TODO: Validate folder path ends in folder name and otherwise throw
+	// 	// an error
+
+	// 	// Get neighbouring folders
+	// 	// (i.e. folders with same currentParentPathDuringTraversal but are only 
+	// 	// one level down)
+	// 	// a
+
+	// 	// Filter OrgIds by whatever the currentParentPathDuringTraversal is
+	// }
 	return []Folder{}
 }
--- main.go
+++ main.go
@@ -1,22 +1,23 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 
 	"github.com/georgechieng-sc/interns-2022/folder"
 	"github.com/gofrs/uuid"
 )
 
 func main() {
-	orgID := uuid.FromStringOrNil(folder.DefaultOrgID)
+	// orgID := uuid.FromStringOrNil(folder.DefaultOrgID)
 
 	res := folder.GetAllFolders()
 
-	// example usage
+	// // example usage
 	folderDriver := folder.NewDriver(res)
-	orgFolder := folderDriver.GetFoldersByOrgID(orgID)
+	// orgFolder := folderDriver.GetFoldersByOrgID(orgID)
+	folderDriver.GetAllChildFolders(uuid.FromStringOrNil("c1556e17-b7c0-45a3-a6ae-9546248fb17a"), "stunning-horridus")
 
-	folder.PrettyPrint(res)
-	fmt.Printf("\n Folders for orgID: %s", orgID)
-	folder.PrettyPrint(orgFolder)
+	// folder.PrettyPrint(res)
+	// fmt.Printf("\n Folders for orgID: %s", orgID)
+	// folder.PrettyPrint(orgFolder)
 }
